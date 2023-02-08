aa7ededbd6065e2f5449826d9f3fe22a1cd99f68
syoungs67 <samuelyoungs67@gmail.com>
2023-02-08 09:07:11 -0800

    exercise 1

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 15;
+        double gpa = 3.45;
+        char fInitial = 'K';
+        char lInitial = 'H';
+        boolean perfectAttendance = true;
+
+        System.out.println(age);
+        System.out.println(gpa);
+        System.out.println(fInitial);
+        System.out.println(lInitial);
+        System.out.println(perfectAttendance);
+    }
+}
