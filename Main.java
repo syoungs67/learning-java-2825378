a27c56aeeb6b24adccb80f7929ddb8719fafd353
syoungs67 <samuelyoungs67@gmail.com>
2023-02-08 10:10:11 -0800

    String join

--- Main.java
+++ Main.java
@@ -2,14 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int age = 15;
         double gpa = 3.45;
-        char fInitial = 'K';
-        char lInitial = 'H';
+        char fInitial = 'I';
+        char lInitial = 'J';
         boolean perfectAttendance = true;
+        String fName = "Indiana";
+        String lName = "Jones";
 
         System.out.println(age);
         System.out.println(gpa);
         System.out.println(fInitial);
         System.out.println(lInitial);
         System.out.println(perfectAttendance);
+        System.out.println(String.join(" ", fName, lName));
     }
 }
