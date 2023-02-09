87113b96ad3c9670b3697e437aaec1420de0dc56
syoungs67 <samuelyoungs67@gmail.com>
2023-02-09 11:05:50 -0800

    reference strings

--- Main.java
+++ Main.java
@@ -2,16 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int age = 15;
         double gpa = 3.45;
-        char fInitial = 'I';
-        char lInitial = 'J';
         boolean perfectAttendance = true;
         String fName = "Indiana";
         String lName = "Jones";
 
         System.out.println(age);
         System.out.println(gpa);
-        System.out.println(fInitial);
-        System.out.println(lInitial);
+        System.out.println(fName.charAt(0));
+        System.out.println(lName.charAt(0));
         System.out.println(perfectAttendance);
         System.out.println(String.join(" ", fName, lName));
     }
