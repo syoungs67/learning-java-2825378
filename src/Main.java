ef09f3a05c4c95caf9507aaf55395952ca2763db
syoungs67 <samuelyoungs67@gmail.com>
2023-02-15 14:43:37 -0800

    add if else statement

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
         System.out.println("Pick a number between 1 and 10");
         Scanner scanner = new Scanner(System.in);
 
-        int inputtedNum = scanner.nextInt();
+        //int inputtedNum = scanner.nextInt();
+        String outString = "You";
+        if (scanner.nextInt() < 5) {
+            outString = String.join(" ", outString, "are a loser man");
+            //outString = "Int less than 5";
+        } else {
+            outString = String.join(" ", outString, "win the big bucks");
+        }
+        System.out.println(outString);
+        scanner.close();
     }
 }
