925e4c4c61faacada42073f800b21b7e73968d64
syoungs67 <samuelyoungs67@gmail.com>
2023-02-15 15:23:37 -0800

    implemented

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String question = "Which of these ships did Roald Amundsen sail to the south pole ?";
+        String w = "a: Gjoa";
+        String x = "b: Fram";
+        String y = "c: Ross";
+        String z = "d: Maud";
+        
+        System.out.println(String.join("\n", question, w, x, y, z));
+        
+        if (scanner.next().equals("b")) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("incorrect");
+        }
+        
     }
 }
