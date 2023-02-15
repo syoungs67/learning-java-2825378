b451050db49684a25234d187845ad2dbc40bd9b6
syoungs67 <samuelyoungs67@gmail.com>
2023-02-15 15:14:20 -0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        boolean isRepeating = true;
-        while(isRepeating) {
-            System.out.println("Playing");
-            System.out.println("Take off repeat? (y/n)");
-            String next = scanner.next();
-
-            if(next.equals("y")) {
-                isRepeating = false;
-            }
-        }
-        scanner.close();
     }
 }
