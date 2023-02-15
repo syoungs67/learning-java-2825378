eb409ff540ade3e3a09fec17aa7df76975f3c00d
syoungs67 <samuelyoungs67@gmail.com>
2023-02-15 14:48:18 -0800

    initial

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
         Scanner scanner = new Scanner(System.in);
-
-        //int inputtedNum = scanner.nextInt();
-        String outString = "You";
-        if (scanner.nextInt() < 5) {
-            outString = String.join(" ", outString, "are a loser man");
-            //outString = "Int less than 5";
-        } else {
-            outString = String.join(" ", outString, "win the big bucks");
-        }
-        System.out.println(outString);
         scanner.close();
     }
 }
