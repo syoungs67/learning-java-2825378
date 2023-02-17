a04aa04b6b72c1e6104c91f1585d52b43ed3e71f
syoungs67 <samuelyoungs67@gmail.com>
2023-02-16 16:39:22 -0800

    init

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,7 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Let's calculate the area of a triangle");
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Please input the base of the triangle (in cm).");
-        double base = input.nextDouble();
-
-        while (base <= 0) {
-            System.out.println("That's invalid. Please input the base of the triangle (in cm).");
-            base = input.nextDouble();
-        }
-
-        System.out.println("Please input the height of the triangle (in cm).");
-        double height = input.nextDouble();
-        while (height <= 0) {
-            System.out.println("That's invalid. Please input the height of the triangle (in cm).");
-            height = input.nextDouble();
-        }
-
-        input.close();
-        double area = (base * height) / 2;
-        System.out.println("The area is " + area);
-
+        
     }
 }
